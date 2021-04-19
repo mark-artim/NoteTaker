f16241a2ac6a3bf1b6ab14ed81b61426bf11357b
Mark Artim <mark.artim@gmail.com>
2021-04-19 16:33:26 -0500

    fix filepaths

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
 
 app.get('/api/notes',(req,res)=>{
     fs.readFile('db/db.json', 'utf8', (err, data) => {
