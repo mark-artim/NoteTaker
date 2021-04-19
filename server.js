ea15851947fe2739634944890ce2faf4972307d4
Mark Artim <mark.artim@gmail.com>
2021-04-19 17:19:16 -0500

    remove Develop folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 // const http = require('http');
 const fs = require('fs');
-const db = require('./Develop/db/db.json');
+const db = require('./db/db.json');
 var uniqid = require('uniqid');
 const { request } = require('http');
 // Sets up the Express App
@@ -16,9 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 app.get('/api/notes',(req,res)=>{
     fs.readFile('db/db.json', 'utf8', (err, data) => {
